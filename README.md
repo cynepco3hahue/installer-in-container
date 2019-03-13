3d479ab7c9755066cacda2f1187834c05ab13a40
Artyom Lukianov <alukiano@redhat.com>
2019-03-13 17:17:19 +0200

    Generate ssh keys under the container

--- README.md
+++ README.md
@@ -16,3 +16,7 @@ NOTE: OpennShift cluster consumes a lot of resources, you should have at least 1
 ***
 
 After that installer done, you can commit the container via `docker commit <container_id> <new_tag>` and re-use it.
+
+### Where to take oc binary
+
+https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
--- images/installer/setup.sh
+++ images/installer/setup.sh
@@ -12,7 +12,10 @@ EOF
 virsh pool-start default
 virsh pool-autostart default
 
-# build installer binary
+## generate ssh key
+mkdir -p /root/.ssh/
+ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
+
 cd $INSTALLER_DIR
 git checkout $INSTALLER_TAG
 git apply /hacks/$INSTALLER_TAG
@@ -23,8 +26,15 @@ if [[ -z "${INSTALLER_PULL_SECRET}" ]]; then
   exit 1
 fi
 
+mkdir -p /root/install
+cp /install-config.yaml /root/install/
+
 # inject pull secret into install config
 echo "pullSecret: '$INSTALLER_PULL_SECRET'" >> /root/install/install-config.yaml
+
+# inject ssh public key into install config
+ssh_pub_key=$(cat /root/.ssh/id_rsa.pub)
+echo "sshKey: '$ssh_pub_key'" >> /root/install/install-config.yaml
 
 # run installer
 OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE=$INSTALLER_RELEASE_IMAGE_OVERRIDE bin/openshift-install create cluster --dir=/root/install
